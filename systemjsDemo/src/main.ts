9e474c94bcf2fc6d47fb3183b7c0bded5bdd50bd
AlexZ33 <zhaokang1@xiaomi.com>
2021-02-25 19:10:38 +0800

    push

---
+++ systemjsDemo/src/main.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-15 11:07:39
+ * @LastEditTime: 2020-10-15 12:03:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /systemjsDemo/src/main.ts
+ */
+import {Person} from './person';
+
+let person = new Person();
+console.log(person.name);
---
+++ systemjsDemo/src/person.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-15 11:07:39
+ * @LastEditTime: 2020-10-15 12:03:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /systemjsDemo/main.ts
+ */
+export class Person {
+  public name: string = 'David';
+}
